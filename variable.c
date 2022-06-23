5b1e1807154f5f87d5e77503c2536979070258b9
SHIV <shivanshguleria@tutanota.com>
2022-06-24 05:19:55 +0530

    variable added

---
+++ variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[]="Josh";
+    int age= 70;
+    printf("His name was %s\n",name);
+    printf("He really liked his name\n");
+    printf("but he did'nt like his age\n");
+    printf("because he was %d\n",age);
+    return 0;
+}
