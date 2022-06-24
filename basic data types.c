f0b3b4284564815cbae04ff7156f5d519bb4fda1
SHIV <shivanshguleria@tutanota.com>
2022-06-24 07:04:10 +0530

    ADDed basc data types file

---
+++ basic data types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 10; //for adding variable
+    float f = 4.6; //for adding decimal numbers
+    float c= 4.0;
+    char d= 'a';  //adding a charachter --Imp--Used to store single alphabet __In single quotes
+    char e[] = "Hello World"; //adding a string in double quotes
+
+    printf("%d\n%f\n%f\n%c\n%s\n",a,f,c,d,e);
+    return 0;
+}
