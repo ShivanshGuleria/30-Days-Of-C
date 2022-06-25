328d9da467501888ce43b13d224ec272f1ccdfed
SHIV <shivanshguleria@tutanota.com>
2022-06-25 06:18:02 +0530

    Added numbers.c

---
+++ numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{ printf("%f\n", 8.9);
+  printf("%f\n",5.89+4);
+  printf("%d\n",54);
+  printf("%d\n",sqrt(1330));
+  printf("%0.0f\n",pow(4, 2));
+  printf("%f\n",ceil(37.5));
+  printf("%f\n",floor(37.5));
+    return 0;
+}
