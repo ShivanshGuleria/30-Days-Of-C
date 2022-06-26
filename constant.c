93aab800bad66a86dacc9e65761ef7bd5cecaf11
ShivanshGuleria <shivanshguleria@tutanota.com>
2022-06-26 23:42:16 +0530

    ADDED constant.c

---
+++ constant.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+ 
+int const a=10;
+const int b=20;
+
+//const variable can also be in capital letters
+
+printf("%d\n%d\n",a,b);
+
+/*In printf statement  text/string inside quotes  is also considered constant */
+
+printf("Insides quotes  I am  A constant");
+printf("\n%d\n",10);
+ return 0;
+
+}
